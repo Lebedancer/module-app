ff46c9b1c1537afcefbf0d49cc4b69785531c206
Andrey Lebedev <lebedancer@gmail.com>
2015-09-07 16:49:02 +0300

    locking table

--- modules/module1/module1View.js
+++ modules/module1/module1View.js
@@ -1,3 +1,33 @@
+let vent;
+
 export default Marionette.LayoutView.extend({
-    template: '#module1Layout-Template'
+    template: '#module1Layout-Template',
+    events: {
+        'click .js-lock': lockTable
+    },
+    ui: {
+        table: '.js-table'
+    },
+    initialize: initialize
 })
+
+/** @access private */
+function initialize(options) {
+    vent = options.vent;
+    bindEvents.call(this);
+}
+
+/** @access private */ 
+function lockTable() {
+
+}
+
+/** @access private */
+function bindEvents() {
+    this.listenTo(vent, 'table:lock', toggleTableLocking)
+}
+
+/** @access private */ 
+function toggleTableLocking() {
+    this.ui.table.toggleClass('disabled');
+}
--- modules/module2/module2View.js
+++ modules/module2/module2View.js
@@ -1,3 +1,19 @@
+let vent;
+
 export default Marionette.LayoutView.extend({
-    template: '#module2View-Template'
+    template: '#module2View-Template',
+    events: {
+        'click .js-lock': lockTable
+    },
+    initialize: initialize
 })
+
+/** @access private */
+function initialize(options) {
+    vent = options.vent;
+}
+
+/** @access private */
+function lockTable() {
+    vent.trigger('table:lock')
+}
